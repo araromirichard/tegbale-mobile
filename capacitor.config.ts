57dd2a4711adced001ff20da2fc8130eacd30d84
karosaxy <araromirichard@gmail.com>
2023-04-09 15:10:00 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'tegbale.mobile',
+  appName: 'tegbale-mobile',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
